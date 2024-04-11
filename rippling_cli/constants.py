4fda577eae12ecfef9b6ab60af8a7d85d2402f90
vguptarippling <vgupta@rippling.com>
2024-04-11 22:09:23 +0530

    Changed client_id to prod client_id

--- rippling_cli/constants.py
+++ rippling_cli/constants.py
@@ -1,7 +1,7 @@
 RIPPLING_DIRECTORY_NAME = "rippling_cli"
 OAUTH_TOKEN_FILE_NAME = "oauth_token.json"
 CODE_CHALLENGE_METHOD = "S256"
-RIPPLING_BASE_URL = "http://localhost:3000"
-RIPPLING_API = "http://localhost:8000"
+RIPPLING_BASE_URL = "https://app.rippling.com"
+RIPPLING_API = "https://app.rippling.com/api"
 EXIT_UNKNOWN_EXCEPTION = 1
 DEFAULT_CODE_VERIFIER_LENGTH = 43
