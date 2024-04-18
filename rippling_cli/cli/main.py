9fed5543cbece117dd967cc5fe685989cc96e83b
vguptarippling <vgupta@rippling.com>
2024-04-18 19:45:27 +0530

    fix imports

--- rippling_cli/cli/main.py
+++ rippling_cli/cli/main.py
@@ -3,7 +3,7 @@ from typing import Union
 
 import click
 
-from rippling_cli.cli.commands.app import app
+from rippling_cli.cli.commands.flux.flux import flux
 from rippling_cli.cli.commands.login import login
 from rippling_cli.config.config import get_client_id, get_oauth_token_data
 from rippling_cli.constants import EXIT_UNKNOWN_EXCEPTION
@@ -43,7 +43,7 @@ def cli(ctx):
 
 COMMANDS_LIST: list[Union[click.Command, click.Group]] = [
     login,
-    app
+    flux
 ]
 
 
