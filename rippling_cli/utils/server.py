311177d6c78748104ad430defda69695be823832
vguptarippling <vgupta@rippling.com>
2024-04-30 17:57:29 +0530

    Update server.py

--- rippling_cli/utils/server.py
+++ rippling_cli/utils/server.py
@@ -7,26 +7,6 @@ from rippling_cli.constants import RIPPLING_API
 from rippling_cli.core.api_client import APIClient
 
 
-def source_venv():
-    """
-    Source the virtual environment based on the platform.
-    :return:
-    """
-    platform = sys.platform
-    if platform == "linux" or platform == "linux2":
-        activate_script = os.path.join(os.getcwd(), "venv/bin/activate")
-    elif platform == "darwin":
-        activate_script = os.path.join(os.getcwd(), "venv/bin/activate")
-    elif platform == "win32":
-        activate_script = os.path.join(os.getcwd(), "venv/Scripts/activate_this.py")
-        exec(open(activate_script).read(), {"__file__": activate_script})
-        return
-    else:
-        raise RuntimeError(f"Unsupported platform: {platform}")
-
-    subprocess.call(f"source {activate_script}", shell=True)
-
-
 def set_forwarding_url(app_install_id, public_url, timeout, oauth_token):
     """
     Set the forwarding URL with the specified timeout.
