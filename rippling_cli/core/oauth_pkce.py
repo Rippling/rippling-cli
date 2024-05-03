1ab173ab632c008bf06bfa199389485ae6087027
vguptarippling <vgupta@rippling.com>
2024-05-03 17:36:00 +0530

    removed unused functions

--- rippling_cli/core/oauth_pkce.py
+++ rippling_cli/core/oauth_pkce.py
@@ -1,23 +1,16 @@
 import pkce
 
-from rippling_cli.constants import CODE_CHALLENGE_METHOD
-
 
 class PKCE:
-    @staticmethod
-    def generate_code_verifier(length=43):
-        code_verifier = pkce.generate_code_verifier(length=length)
-        return code_verifier
-
-    @staticmethod
-    def get_code_challenge(code_verifier, method=CODE_CHALLENGE_METHOD):
-        if method == CODE_CHALLENGE_METHOD:
-            code_challenge = pkce.get_code_challenge(code_verifier)
-            return code_challenge
-        else:
-            raise ValueError("Unsupported code challenge method")
-
+    """
+    PKCE class for generating PKCE code verifier and code challenge
+    """
     @staticmethod
     def generate_pkce_pair(code_verifier_length=43):
+        """
+        Generate PKCE code verifier and code challenge pair
+        :param code_verifier_length:
+        :return:
+        """
         code_verifier, code_challenge = pkce.generate_pkce_pair(code_verifier_length)
         return code_verifier, code_challenge
